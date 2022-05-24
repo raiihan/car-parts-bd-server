7d493991e178694a6437d78e8aa36c452238fbce
raiihan <s.siilavn@yahoo.com>
2022-05-24 15:31:21 +0600

    add review

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json())
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cars-parts.ncua4.mongodb.net/?retryWrites=true&w=majority`;
@@ -17,10 +20,20 @@ async function run() {
         await client.connect();
 
         const partsCollencton = client.db('carParts').collection('parts');
+        const reviewCollencton = client.db('carParts').collection('reviews');
 
         app.get('/parts', async (req, res) => {
             const parts = await partsCollencton.find().toArray();
             res.send(parts)
+        })
+
+
+        // Review
+
+        app.post('/review', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollencton.insertOne(review);
+            res.send(result)
         })
 
     } finally { }
