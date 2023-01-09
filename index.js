dd6c1a16aea3747100a8d460f18c8ffe06b51ddb
raiihan <s.siilavn@yahoo.com>
2023-01-09 10:31:25 +0600

    server change

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+    });
+ 
+
 // verify Jsonwebtoken
 function verifyJWToken(req, res, next) {
     const authHeader = req.headers.authorization;
