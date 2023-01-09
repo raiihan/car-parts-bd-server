641904f2252e3afadc8233173d3f3117ea8ab768
raiihan <s.siilavn@yahoo.com>
2023-01-09 10:54:20 +0600

    server change

--- index.js
+++ index.js
@@ -12,13 +12,18 @@ app.use(cors());
 app.use(express.json())
 
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+// app.use(function (req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     next();
+//     });
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
     next();
-    });
+  });
  
 
 // verify Jsonwebtoken
